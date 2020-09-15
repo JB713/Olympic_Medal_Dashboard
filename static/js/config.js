0ce832ce1ebbe8bb0d00811ee2c430eacb1f8a1b
p4rc1v4l <mlastudy.riceu@gmail.com>
2020-09-15 01:50:33 -0500

    Adding functionality to show number of medals per country in a leaflet map

---
+++ static/js/config.js
@@ -0,0 +1,1 @@
+const API_KEY = 'pk.eyJ1IjoibWxhc3R1ZHlyaWNldSIsImEiOiJja2VhbGN0YTMwMXBmMzVtbnVwNWZxZzczIn0.aNAd9VfIs_nZWuVOvBKlOQ';
--- static/js/medals_map.js
+++ static/js/medals_map.js
@@ -14,7 +14,7 @@ L.tileLayer(
     {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         id: "light-v10",
-        accessToken: process.env.API_KEY
+        accessToken: API_KEY
     }
 ).addTo(worldMap);
 
