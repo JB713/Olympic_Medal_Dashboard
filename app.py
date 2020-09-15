2506adcba0cd9d5b31984eeac91419968210251f
p4rc1v4l <mlastudy.riceu@gmail.com>
2020-09-15 15:50:36 -0500

    reading api key from env variabe

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 from Services import Services
 from flask import Flask, jsonify, render_template
+from config import LOCAL_API_KEY
+import os
 
 # create instance of Flask app
 app = Flask(__name__)
@@ -15,6 +17,11 @@ data = Services()
 def index():
     return render_template('index.html')
 
+
+@app.route("/api/v1.0/get_api_key")
+def get_api_key():
+    # return os.environ.get('HEROKU_MAP_KEY', '') or LOCAL_API_KEY;
+    return LOCAL_API_KEY;
 
 @app.route("/api/v1.0/medals")
 def get_all_medals():
--- static/js/medals_map.js
+++ static/js/medals_map.js
@@ -8,7 +8,10 @@ const worldMap = L.map(
     }
 );
 
-// Create the tile layer that will be the background of our map
+
+    d3.json('/api/v1.0/get_api_key').then(data => {
+        const API_KEY = data
+ // Create the tile layer that will be the background of our map
 L.tileLayer(
     "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
     {
@@ -17,6 +20,8 @@ L.tileLayer(
         accessToken: API_KEY
     }
 ).addTo(worldMap);
+    });
+
 
 const GOLD_COLOR = "#FFD700";
 const SILVER_COLOR = "#c0c0c0";
